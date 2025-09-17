0eff443c70c4c4e1bffcf68beb89f9f166a30645
Nino <nino2000kapanadze@gmail.com>
2025-09-17 19:13:50 +0400

    added mvc project

---
+++ PersonManagement.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonManagement.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
